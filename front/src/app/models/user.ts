660534f0f9578af232cd3bfe8bd5992ae5ec54f5
MurilloYamasaki <cracker021@hotmail.com>
2019-04-29 02:51:42 -0300

    User Model Class

---
+++ front/src/app/models/user.ts
@@ -0,0 +1,15 @@
+import { Binary } from 'selenium-webdriver/firefox';
+
+export class User {
+    // incomplete!!!--------------------
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: boolean;
+    photo: string;
+    bio: string;
+    email: string;
+    password: string;
+    nickname: string;
+    enabled: boolean;
+}
