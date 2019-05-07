d0deb24b9a2424f11c275bf24819489d1e2f5ec4
MurilloYamasaki <cracker021@hotmail.com>
2019-05-07 17:29:43 -0300

    Some Fixes in Login
    Incomplete

--- front/src/app/services/user.service.ts
+++ front/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,15 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   listUsers(): Observable<User[]> {
-    return this.http.get<User[]>('http://localhost:8080/api/user');
+    return this.http.get<User[]>('http://localhost:8080/tooeater/api/user');
   }
 
   addUser(user: User): Observable<any> {
-    return this.http.post('http://localhost:8080/api/user', user);
+    return this.http.post('http://localhost:8080/tooeater/api/user', user);
+  }
+
+  userAuthentication(email,password){
+    return this.http.post('http://localhost:8080/tooeater/api/auth', {email, password});
   }
 
 }
--- front/src/app/views/user/sign-in/sign-in.component.ts
+++ front/src/app/views/user/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  isLoginError : boolean = false;
+  constructor(private userService : UserService, private router : Router) { }
 
   ngOnInit() {
+  }
+
+  OnSubmit(email,password){
+    this.userService.userAuthentication(email,password).subscribe((data : any)=>{
+      localStorage.setItem('token',data.acess_token);
+      this.router.navigate(['/home']);
+    },
+    (err : HttpErrorResponse)=>{
+      this.isLoginError = true;
+    });
   }
 
 }
